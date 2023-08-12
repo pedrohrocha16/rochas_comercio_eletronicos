45b5b7799a60def63179054064294aa22ffc037d
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-08-12 14:15:36 -0300

    Corrigindo erro

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <title>Rocha's e-commerce</title>
       <Head />
       <body>
         <Main />
