d6fcbe2df386859932d09b73df8a6d50beffdea4
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-08-12 14:28:42 -0300

    Desativando Eslint

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -5,7 +5,7 @@ type Props = {}
 export default function Footer({ }: Props) {
    return (
       <div className='footer select-none'>
-         &copy;2023 - Rocha's e-commerce
+         <p>&copy;2023 - Rocha's e-commerce</p>
       </div>
    )
 }
