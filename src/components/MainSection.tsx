fd0bfeac42f6acf15d94deae3958dbd65f0fed94
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-08-14 15:06:18 -0300

    ..

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -19,6 +19,7 @@ export default function MainSection({ }: Props) {
         </div>
         <div className='flex flex-col'>
           <MainList />
+          <MainList />
         </div>
       </main>
     </div>
