1873572e7567095b4c3afe26bc08a4efe0edfa63
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-09-28 09:27:58 -0300

    Fix: corrigindo arquivo store

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,7 +1,6 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
 import rootReducer from './root-reducer'
-import logger from 'redux-logger'
 
-const store = createStore(rootReducer, applyMiddleware(logger))
+const store = createStore(rootReducer)
 
 export default store
